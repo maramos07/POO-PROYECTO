c7b3db143b598bb707cd42697681475cf764d180
Maria <mramosnaizir@gmail.com>
2026-06-19 11:45:38 -0500

    hola munod

---
+++ src/main/java/poo/Main.java
@@ -0,0 +1,13 @@
+package poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("MI PROYECTO."));
+
+
+    }
+}
